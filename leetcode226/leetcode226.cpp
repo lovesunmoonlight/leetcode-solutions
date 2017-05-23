f8233c16de953ff4c898e95afbe4c20f1189331a
lovesunmoon <lovesunmoonlight@gmail.com>
2017-05-23 17:41:59 +0800

    2017.5.23

---
+++ leetcode226/leetcode226.cpp
@@ -0,0 +1,44 @@
+// leetcode226.cpp : 定义控制台应用程序的入口点。
+// 翻转二叉树
+// 层次搜索左右子树并交换
+// Accepted
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+/**
+* Definition for a binary tree node.
+* 
+*/
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	TreeNode* invertTree(TreeNode* root) {
+		if (!root)
+			return nullptr;
+		TreeNode* temp = root->left;
+		root->left = invertTree(root->right);
+		root->right = invertTree(temp);
+		//travePre(root);
+		return root;
+	}
+};
+
+int main()
+{
+	system("pause");
+	return 0;
+}
+
